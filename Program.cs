e942dff983715de34b14bd111dc9d75632774dfe
unknown <vasanthgamer2000@gmail.com>
2023-07-13 09:45:59 +0530

    Exercise 1 Day 4

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pre = 60;
+            int final = 55;
+            int marks;
+
+            Console.WriteLine("Enter the Pre Final Marks : ");
+            marks = int.Parse(Console.ReadLine());
+
+            if (pre < marks)
+            {
+                if (marks > final)
+                {
+                    Console.WriteLine("Selected");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fail in Pre");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
